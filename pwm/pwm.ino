4f75709dc3398b6dc79e617e3cde1f0c6492697c
rdgoite <rdgoite@gmail.com>
2015-07-26 19:48:31 +0800

    Added push button controls.

--- pwm/pwm.ino
+++ pwm/pwm.ino
@@ -1,22 +1,43 @@
 const int LED = 9;
+const int BUTTON = 7;
 
 const int MIN_ANALOG = 0;
 const int MAX_ANALOG = 255;
 
 const int DELAY = 10;
 
+int input = 0;
+int oldInput = input;
+int state = 0;
+
+int brightness = 128;
+unsigned long startTime = 0;
+
 void setup() {
   pinMode(LED, OUTPUT);
+  pinMode(BUTTON, INPUT);
 }
 
 void loop() {
-  for (int level = MIN_ANALOG; level < MAX_ANALOG; level++) {
-    analogWrite(LED, level);
+  input = digitalRead(BUTTON);
+  
+  if (input == HIGH && oldInput == LOW) {
+    state = 1 - state;
+    startTime = millis();
     delay(DELAY);
   }
 
-  for(int level = MAX_ANALOG; level > MIN_ANALOG; level--) {
-    analogWrite(LED, level);
-    delay(DELAY);
+  if ((input == HIGH && oldInput == HIGH) && (state == 1 && ((millis() - startTime) > 500))) {
+    brightness++;
+    delay(10);
+    if (brightness > MAX_ANALOG) brightness = 0;
+  }
+
+  oldInput = input;
+
+  if (state == HIGH) {
+    analogWrite(LED, brightness);
+  } else {
+    analogWrite(LED, LOW);
   }
 }
